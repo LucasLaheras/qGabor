a351efa24ca66f9c676bdbaa1651e017eecd0ebe
Lucas Laheras <lucaslaheras@hotmail.com>
2020-07-21 00:29:37 -0300

    qGabor organized in class

--- q_gabor.py
+++ q_gabor.py
@@ -1,51 +1,60 @@
 import tensorflow as tf
 import numpy as np
 
-eps = np.finfo(float).eps
+
+class q_gabor():
+    def __init__(self, q, alpha=0.3, f=0.08, theta=0, k=1):
+        """"
+            :param alpha:
+            :param q: opening
+            :param f:
+            :param theta: angle
+            :param k:
+        """
+        self.q = q
+        self.alpha = alpha
+        self.f = f
+        self.theta = theta
+        self.k = k
+
+    def sinusoidal_function(self, f, x):  # s(X) = e^(2*π*f*X*i)
+        s = pow(np.e, 2 * np.pi * f * x * 1j)
+        return s
+
+    def q_exponential_function(self, x, q):  # w(X) =1/(1+(1-q)*X^2)^(1/(1-q))
+        w = 1 / pow(1 + (1 - q) * x * x, 1 / (1 - q))
+        return w
+
+    def q_gabor1d(self, x, alpha, q, f, theta, k):  # g(X)=k*e^(theta*i)*w(alpha*X)*s(X)
+        x = tf.cast(x, dtype=tf.complex128)
+        sinusoidal = self.sinusoidal_function(f, alpha * x)
+        q_exponencial = self.q_exponential_function(x, q)
+        g = k * pow(np.e, (theta * 1j)) * sinusoidal * q_exponencial
+        g = tf.cast(g, dtype=tf.float32)
+        return g
+
+    def q_gabor_2d(self, x, y, q, k, u, v, p, a, b):
+        """"
+            :param x = data
+            :param y = data
+            :param q = opening
+            :param k = amplitude
+            :param u = X filter frequency
+            :param v = Y filter frequency
+            :param p = filter phase
+            :param a = envelope
+            :param b = envelope
+        """
+        xo = yo = 0
+        w = k * (1 / ((1 + (1 - q) * ((a ** 2 * (x - xo) ** 2 + b ** 2 * (y - yo) ** 2))) ** (1 / (1 - q)))) #<- formula diferente!!!!
+        s = np.exp((2 * np.pi * (u * x + v * y) + p) * 1j)
+        g = w * s
+        return g
+
+    def qGaborActivation(self, x):
+        """"
+            :param x: data
+        """
+        return self.q_gabor1d(x, self.alpha, self.q, self.f, self.theta, self.k)
 
 
-# s(x)
-def sinusoidal_calc(f, x):
-    s = np.exp((2 * np.pi * f * x) * 1j)
-    return s
-
-
-# w(x)
-def q_exponential_calc(x, q):
-    base = 1 + (1 - q) * pow(x, 2)
-    w = 1 / pow(base, 1 / (1 - q))
-    return w
-
-
-def q_gabor_1d(x, alpha, q, f, theta, k):
-    """""
-        x = data
-        alpha = 
-        q = opening
-        f = 
-        theta = angle
-        k = 
-    """""
-
-    sinusoidal = sinusoidal_calc(x, f)
-    q_exponencial = q_exponential_calc(alpha * x, q)
-    potentiation = pow(eps, theta * 1j).real
-    g = k * potentiation * sinusoidal * q_exponencial
-    return g
-
-
-def q_gabor_2d(x, y, q, k, u, v, p, a, b):
-    """""
-        x and y = data
-        q = opening
-        k = amplitude
-        u and v = filter frequency
-        p = filter phase
-        a and b = envelope 
-    """""
-
-    xo = yo = 0
-    w = k * (1 / ((1 + (1 - q) * ((a ** 2 * (x - xo) ** 2 + b ** 2 * (y - yo) ** 2))) ** (1 / (1 - q)))) #<- formula diferente!!!!
-    s = np.exp((2 * np.pi * (u * x + v * y) + p) * 1j)
-    g = w * s
-    return g
